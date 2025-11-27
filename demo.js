bbabf90afb65e16b87a760f55e44119cd4c49fa2
Andre Tavara <andretj_92@live.com>
2025-11-26 21:07:42 -0500

    JavaScript: datos primitivos y variables

---
+++ demo.js
@@ -0,0 +1,39 @@
+// Numbers
+// console.log(999);
+// console.log(0);
+// console.log(-20);
+// console.log(0.5);
+// console.log(-2.5);
+
+// Strings
+// console.log("Hola Mundo!");
+// console.log(`backticks`);
+// console.log("A");
+// console.log(""); // string vacio
+// console.log("!@#$%^&*_)*");
+// console.log("");
+
+// Booleans
+// console.log(true);
+// console.log(false);
+
+// Null
+// console.log(null);
+
+// Undefined
+// console.log(undefined);
+
+// Variables
+// let email = "andre@mail.com";
+// console.log(email);
+// email = "andre_tavara@mail.com";
+// console.log(email);
+
+// edad = 28; Error!
+let a = 100;
+let b = a;
+console.log(a);
+console.log(b);
+a = 10;
+console.log(a);
+console.log(b);
