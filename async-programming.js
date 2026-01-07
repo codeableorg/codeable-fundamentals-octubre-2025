2f3f5607f7e1c51f17781cc055fe1501c6a15c93
Andre Tavara <andretj_92@live.com>
2026-01-06 21:01:13 -0500

    Agrega clase de callbacks

--- async-programming.js
+++ async-programming.js
@@ -11,26 +11,72 @@ const users = [
     name: "Ana",
     githubUsername: "anita-dev",
   },
+  {
+    email: "codeable@mail.com",
+    password: "letmein",
+    name: "Codeable",
+    githubUsername: "codeableorg",
+  },
 ];
 
-function getUser(email, password) {
-  const user = users.find((user) => user.email === email); // validar correo
-  // cuando user guarda undefined
-  // if user es undefined, entonces lanzamos el error
-  if (user === undefined || user.password !== password) {
-    throw new Error("Credenciales invalidas");
-  }
+const githubPublicRepos = [
+  { id: 1, owner: "pepe-20", name: "hello-world" },
+  { id: 2, owner: "pepe-20", name: "say-hi" },
+  { id: 3, owner: "anita-dev", name: "first-repo" },
+  { id: 4, owner: "anita-dev", name: "codeable" },
+  { id: 5, owner: "anita-dev", name: "example" },
+  { id: 6, owner: "codeableorg", name: "codeable-fundamentals-octubre-2025" },
+];
 
-  return user;
+function getUser(email, password, callback) {
+  setTimeout(() => {
+    const user = users.find((user) => user.email === email);
+    if (user === undefined || user.password !== password) {
+      // escenario malo
+      const error = new Error("Credenciales invalidas");
+      callback(error);
+      return;
+    }
+
+    callback(null, user); // escenario bueno
+  }, 2000);
 }
 
-const user = getUser("ana@mail.com", "letmein");
-console.log("¡Bienvenid@ " + user.name + "!"); // ¡Bienvenid@ Ana!
-const user5 = getUser("pepe@mail.com", "supersecret");
-console.log("¡Bienvenid@ " + user5.name + "!"); // ¡Bienvenid@ Pepe!
-// const user2 = getUser("pepe@mail.com", "123456");
-// console.log("¡Bienvenid@ " + user2.name + "!"); // Credenciales invalidas
-// const user3 = getUser("andre@mail.com", "supersecret");
-// console.log("¡Bienvenid@ " + user3.name + "!"); // Credenciales invalidas
-// const user4 = getUser("luis@mail.com", "asdfgh");
-// console.log("¡Bienvenid@ " + user4.name + "!"); // Credenciales invalidas
+function getPublicRepos(username, callback) {
+  setTimeout(() => {
+    // Simulamos un posible error de conexión con Github
+    const hasConnectionError = Math.random() > 0.9; // 90% false 10% true
+    if (hasConnectionError) {
+      const error = new Error("Error de conexión");
+      callback(error);
+      return;
+    }
+
+    const repos = githubPublicRepos.filter((repo) => repo.owner === username);
+
+    callback(null, repos);
+  }, 2000);
+}
+
+const email = prompt("Email:");
+const password = prompt("Password:");
+
+// funciones asincronas
+getUser(email, password, function (error, user) {
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+
+  console.log("¡Bienvenid@ " + user.name + "!");
+
+  getPublicRepos(user.githubUsername, function (error, repos) {
+    if (error) {
+      console.log(error.message);
+      return;
+    }
+
+    console.log("- Lista de proyectos:");
+    repos.forEach((repo) => console.log(repo.name));
+  });
+});
